1103c9e34a7853b46b685d0815ae21e769a3663f
Muhammad Gibran <gibramuh@gmail.com>
2023-07-07 11:07:13 +0700

    test

--- src/Http/Controllers/LoginDigiForSDIController.php
+++ src/Http/Controllers/LoginDigiForSDIController.php
@@ -12,8 +12,8 @@ class LoginDigiForSDIController extends Controller
 {
     public function index(Request $request)
     {
-     dd("yoyoyo pidnah");
-     return view('vendor.login.index'); 
+        
+     return view('index'); 
 
     }
 
