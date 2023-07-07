2a9cc5cce9c042770cc2469b302ee61f92af5b56
Muhammad Gibran <gibramuh@gmail.com>
2023-07-07 14:35:58 +0700

    test login bstar page

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,8 @@ use Illuminate\Support\Facades\Route;
 use Smpl\Logindigiforsdi\Http\Controllers\LogindigiforsdiController;
 
 
-    Route::get('/', function () {
-        return view('login::index');
-    })->name('login');
+Route::get('/', function () {
+    return view('login::index');
+})->name('login');
+
+Route::post('/auth', [APIController::class,'auth'])->name('login-auth');
