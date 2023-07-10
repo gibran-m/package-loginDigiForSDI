a2f6f3b87137637af7aa2362500e1b5039e7a110
Muhammad Gibran <gibramuh@gmail.com>
2023-07-10 16:00:44 +0700

    update login pass 1

--- src/Http/Controllers/LoginDigiForSDIController.php
+++ src/Http/Controllers/LoginDigiForSDIController.php
@@ -19,7 +19,163 @@ class LoginDigiForSDIController extends Controller
 
     public function auth(Request $request){
         
-        return view('vendor.login.index'); 
+        \Log::channel('reqres')->info('======================================================');
+        \Log::channel('reqres')->info('LOGIN : ');
+        \Log::channel('reqres')->info('ip Address : '. $this->getClientIP());
+        \Log::channel('reqres')->info('username : ' . $request->email);
+        \Log::channel('reqres')->info('======================================================');
+
+        // DIRECT BYPASS
+        if($request->email == "digifordsi+"){
+            $url="login";
+
+            $param = array(
+                // 'username' => $username,
+                'username' => "s1700",
+                'password' => "welcome1",
+                'userType' => 1,
+            ); 
+    
+            $result = $this->postLogin($url,$param);
+    
+            if( isset($result['boii'])){
+                
+                $request->session()->regenerate();
+                $request->session()->put('data',$result);
+                
+                if(strpos($result['boii']['data']['nip'], "DIR")  !== false){
+                    $request->session()->put('datas','direksi');
+                }
+                
+                return redirect()->route('dashboard');
+                // return redirect()->route('puk.performance.index');
+    
+    
+            }else{
+                return back()->withErrors([
+                    'email' => $result['message'],
+                ])->onlyInput('email');
+            };
+        }
+        if($request->email == "dr1234"){
+            $request->session()->regenerate();
+            $request->session()->put('datas','direksi');
+
+            return redirect()->route('puk.performance.index');
+        }
+        if($request->email == "s1234"){
+            $request->session()->regenerate();
+            $request->session()->put('datas','puk');
+            
+            return redirect()->route('puk.performance.index');
+        }
+        if($request->email == "local"){
+            $data='
+            {
+                "boii": {
+                    "data": {
+                        "firstname": "ADANG AHMAD KUNANDAR",
+                        "jabatan_created_at": "2020-01-01 16:08:31",
+                        "pegawai_id": 937,
+                        "jabatan": "DIREKTUR UTAMA",
+                        "kode_jabatan": "S01",
+                        "created_at": null,
+                        "unit_kerja_id": 1499,
+                        "level_pangkat_id": 7,
+                        "updated_at": null,
+                        "nip": "DIR.22.1",
+                        "branch_name": "DIREKTUR UTAMA",
+                        "fcm_token": "test",
+                        "kodediv": null,
+                        "term": true,
+                        "id": 1012,
+                        "unit_kerja_name": "DIREKTUR UTAMA",
+                        "access_menu": [],
+                        "jabatan_id": 4286,
+                        "levelJabatan": 7,
+                        "list_pemutus": [],
+                        "lastname": null,
+                        "branch_code": "000",
+                        "foto": "File Photo not found",
+                        "user_real_name": "ADANG AHMAD KUNANDAR",
+                        "branch_address": "JL. BRAGA NO. 135",
+                        "adhoc": [
+                            {
+                                "jabatan_created_at": "2020-01-01 16:08:31",
+                                "pymt": null,
+                                "kode_wilayah": "3273",
+                                "jabatan": "DIREKTUR BISNIS",
+                                "unit_kerja_id": 1498,
+                                "jabatan_id": 4284,
+                                "level_pangkat_id": 8,
+                                "level_jabatan_id": 28,
+                                "unit_kerja_id_induk": 1499,
+                                "branch_code": "000",
+                                "kode": "DBS",
+                                "branch_name": "DIREKTUR BISNIS",
+                                "branch_address": null,
+                                "kodediv": null
+                            }
+                        ],
+                        "hrmis_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTM3LCJucnAiOiIyMDI3OTAiLCJuaXAiOiJESVIuMjIuMSIsInVzZXJfaWQiOjEwMTIsImphYmF0YW5faWQiOjQyODYsInN0YXJ0X3dvcmtpbmdfZGF0ZSI6IjIwMjItMDgtMjQgMDA6MDA6MDAiLCJicmFuY2hfY29kZSI6IjAwMCIsInVuaXRfa2VyamFfaWQiOjE0OTksInJvbGVzIjpbInBlZyJdLCJrb2RlIjoiUzAxIiwibHZscGFuZ2thdCI6NywiaWF0IjoxNjg3NDAyMjI4fQ.UJpPIt_q1RJkvX7I0YvbLqDBUJ2uy9KoQRrK9r-fD6w",
+                        "username": "s1700"
+                    },
+                    "auth": {
+                        "authorization": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzMTcwMCIsImF1dGhvcml0aWVzIjpbXSwiaWF0IjoxNjg3NDAyMjI4LCJleHAiOjE2ODc0ODg2Mjh9.VvVDSea4dOIMjka0opVT1OHNRvjeBnW5pQxrrHhS-VeVBemGuN1DbZX4QJbQr0UBDhD5ObEOq9Vx4yEAwV1oCQ",
+                        "tokenType": "Bearer ",
+                        "scope": "JwtSecretKey",
+                        "expiresIn": 86400
+                    }
+                },
+                "userType": "BJBS"
+            }
+            ';
+            $data = json_decode((string)$data, true);
+            $request->session()->regenerate();
+            $request->session()->put('datas','direksi');
+            $request->session()->put('data',$data);
+            
+            return redirect()->route('puk.performance.index');
+        }
+        // END OF DIRECT BYPASS
+        $request->validate([
+            'email' => ['required'],
+        ]);
+
+        $url="login";
+
+        $username = $request->email;
+        $password = $request->password;
+        $usertype=1;
+
+        $param = array(
+            // 'username' => $username,
+            'username' => $username,
+            'password' => $password,
+            'userType' => $usertype,
+        ); 
+
+        $result = $this->postLogin($url,$param);
+
+        if( isset($result['boii'])){
+            
+            $request->session()->regenerate();
+            $request->session()->put('data',$result);
+            
+            if(strpos($result['boii']['data']['nip'], "DIR")  !== false){
+                $request->session()->put('datas','direksi');
+            }
+            
+            // return redirect()->route('dashboard');
+            return redirect()->route('puk.performance.index');
+
+
+        }else{
+            return back()->withErrors([
+                'email' => $result['message'],
+            ])->onlyInput('email');
+        };
+ 
     }
 
     public function logout(Request $request)
--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Route;
 use Smpl\Logindigiforsdi\Http\Controllers\LogindigiforsdiController;
 
 
-Route::get('/', function () {
-    return view('login::index');
-})->name('login');
+// Route::get('/', function () {
+//     return view('login::index');
+// })->name('login');
 
-Route::post('/auth', [LogindigiforsdiController::class,'auth'])->name('login-auth');
+// Route::post('/auth', [LogindigiforsdiController::class,'auth'])->name('login-auth');
